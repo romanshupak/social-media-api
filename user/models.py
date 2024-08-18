3b6504b3d1760dfe91887c7209dfb6ddee843370
Roman Shupak <romanshupak808@gmail.com>
2024-08-18 17:00:08 +0300

    fix migrations

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
+import os
+import uuid
+
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
 )
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
 
@@ -41,13 +45,18 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    username = None
     email = models.EmailField(_("email address"), unique=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 
 # def avatar_image_file_path(instance, filename):
