544aeef2c65b9c6bd1bb89c67a3f8e3d701f1f01
Roman Shupak <romanshupak808@gmail.com>
2024-08-18 17:55:35 +0300

    create serializers.py and admin.py, create media folder, add static settings in user/urls.py

--- user/models.py
+++ user/models.py
@@ -59,8 +59,8 @@ class User(AbstractUser):
         return self.email
 
 
-# def avatar_image_file_path(instance, filename):
-#     _, extension = os.path.splitext(filename)
-#     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
-#
-#     return os.path.join("uploads/shows/", filename)
+def avatar_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/avatars/", filename)
