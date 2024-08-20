ccf395421b2bb10934e1b2bcad8426b90cced014
Roman Shupak <romanshupak808@gmail.com>
2024-08-20 13:25:55 +0300

    Fix media_api/views.py in order to obtain correct endpoint navigation. Add migrations for models Post, Comment, Like.

--- media_api/models.py
+++ media_api/models.py
@@ -14,20 +14,20 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.author.email} - {self.content[:30]}'
+        return f"{self.author.email} - {self.content[:30]}"
 
 
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.email} liked {self.post.id}'
+        return f"{self.user.email} liked {self.post.id}"
--- media_api/views.py
+++ media_api/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.viewsets import GenericViewSet
 
 from media_api.models import Post, Comment, Like
-from media_api.serializers import PostSerializer, CommentSerializer, LikeSerializer
+from media_api.serializers import (
+    PostSerializer,
+    CommentSerializer,
+    LikeSerializer,
+)
 
 
-class PostViewSet(GenericViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class CommentViewSet(GenericViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 
-class LikeViewSet(GenericViewSet):
+class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
